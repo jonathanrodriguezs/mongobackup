e6290008352ba659522005b38575e3703b30b1ad
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2021-03-24 01:17:54 -0600

    feat: add main methods to BackupService class

--- src/BackupService.ts
+++ src/BackupService.ts
@@ -1,8 +1,31 @@
-interface IBackupService {
+import path from 'path'
+import { spawn } from 'child_process'
+import { Utils } from './Utils'
+
+export interface IBackupService {
   getListOfSnapshots(): Promise<Array<string[]>>
+  createSnapshot(database: string): Promise<void>
 }
 
 export class BackupService implements IBackupService {
+  protected path: string
+  private utils: Utils
+
+  constructor() {
+    this.path = path.resolve(__dirname, 'backups')
+    this.utils = new Utils()
+    this.utils.createDirectory(this.path)
+  }
+
+  async createSnapshot(database: string): Promise<void> {
+    const timestamp: number = new Date().getTime()
+    const directory = path.join(this.path, database)
+    const filepath: string = path.join(directory, timestamp + '.dump')
+
+    await this.utils.createDirectory(directory)
+    spawn('mongodump', ['-d=' + database, '--archive=' + filepath])
+  }
+
   async getListOfSnapshots(): Promise<Array<string[]>> {
     try {
       return [
